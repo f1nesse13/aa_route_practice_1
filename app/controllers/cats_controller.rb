cd4cfbb81974d7232575492d5d794fe1a2ec3d90
Joe Pell <jpell3130@gmail.com>
2019-04-19 20:08:00 -0400

    methods for basic cat routes implesmented

--- app/controllers/cats_controller.rb
+++ app/controllers/cats_controller.rb
@@ -9,8 +9,37 @@ class CatsController < ApplicationController
   end
 
   def create
+    cat = Cat.new(cat_params)
+    if cat.save
+      render json: cat
+    else
+      render json: cat.errors.full_message, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    cat = Cat.find_by({ id: params[:id] })
+
+    if cat.update(cat_params)
+      render json: cat
+    else
+      render json: cat.errors.full_message, status: :unprocessable_entity
+    end
   end
 
   def destroy
+    cat = Cat.find_by({ id: params[:id] })
+
+    if cat.destroy
+      render json: cat
+    else
+      render json: cat.errors.full_message, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def cat_params
+    params.require(:cats).permit(:name, :age)
   end
 end
